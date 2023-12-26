2219c3ea1230675c14a16bbc83b38d7775c77bbc
moligarch <mr.verkiani@gmail.com>
2023-12-26 19:09:04 +0330

    [UPD] Base Stable Version

--- ArchiveSniper/ArchiveSniper.cpp
+++ ArchiveSniper/ArchiveSniper.cpp
@@ -11,43 +11,20 @@ ArcSnp::~ArcSnp()
 {
 }
 
-META ArcSnp::GetMetadata(const std::string& filePath, const std::string& logFilePath) {
+META ArcSnp::GetMetadata(const std::string& filePath) {
 
-    std::ofstream log(logFilePath);
     try { // bit7z classes can throw BitException objects
 
         bit7z::BitArchiveReader arc{ *mLib, filePath, bit7z::BitFormat::Auto};
 
-        META result{ arc.itemsCount(),arc.foldersCount(), arc.filesCount(), arc.size(), arc.packSize(), arc.format().value() };
-        // Printing archive metadata
-        log << "Archive properties" << std::endl;
-        log << "  Items count: " << result.itemsCount << std::endl;
-        log << "  Folders count: " << result.foldersCount << std::endl;
-        log << "  Files count: " << result.filesCount << std::endl;
-        log << "  Size: " << result.size << std::endl;
-        log << "  Packed size: " << result.packSize << std::endl;
-        log << "  Packed size: " << result.format << std::endl << std::endl;
-
-        // Printing the metadata of the archived items
-        std::cout << "Archived items";
-        auto arc_items = arc.items();
-        for (auto& item : arc_items) {
-            log << std::endl;
-            log << "  Item index: " << item.index() << std::endl;
-            log << "    Name: " << item.name() << std::endl;
-            log << "    Extension: " << item.extension() << std::endl;
-            log << "    Path: " << item.path() << std::endl;
-            log << "    IsDir: " << item.isDir() << std::endl;
-            log << "    Size: " << item.size() << std::endl;
-            log << "    Packed size: " << item.packSize() << std::endl;
-            log << "    CRC: " << item.crc() << std::endl;
-        }
+        META result{ arc.itemsCount(),arc.foldersCount(), arc.filesCount(), arc.size(), arc.packSize(), arc.format().value(), std::error_code{} , "" };
 
         return result;
     }
-    catch (const bit7z::BitException& ex) { 
-        log << ex.what();
-        return META{};
+    catch (const bit7z::BitException& ex)
+    {
+        META failed{ NULL,NULL,NULL,NULL,NULL,NULL,ex.code(),ex.what()};
+        return failed;
     }
 }
 
@@ -66,10 +43,9 @@ content_t ArcSnp::GetContent(std::string& path)
             bit7z::buffer_t buffer{};
             arc.extractTo(buffer, item.index());
             result.emplace_back(DCOMP{
-                                path + "->" + item.path(),
-                                kBaseArchiveLevel,
-                                item.size(),
-                                buffer });
+                path + "->" + item.path(),
+                kBaseArchiveLevel,
+                buffer });
         }
     }
     else
@@ -77,12 +53,19 @@ content_t ArcSnp::GetContent(std::string& path)
         bit7z::BitArchiveReader arc{ *mLib, path, bit7z::BitFormat::Auto };
         auto arc_items = arc.items();
         for (const auto& item : arc_items) {
-            if (ValidateFile(item))
+            bit7z::buffer_t tmpBuffer{};
+            arc.extractTo(tmpBuffer, item.index());
+            if (CheckBuffer(item))
             {   
-                bit7z::buffer_t tmpBuffer{};
-                arc.extractTo(tmpBuffer, item.index());
                 auto output = ResolveBuffer(path + "->" + item.path(), tmpBuffer);
                 result.insert(result.end(), output.begin(), output.end());
+            }
+            else
+            {
+                result.emplace_back(DCOMP{
+                    path + "->" + item.path(),
+                    mDepth,
+                    tmpBuffer });
             }
         }
     }
@@ -106,32 +89,49 @@ content_t ArcSnp::ResolveBuffer(const std::string& basePath, bit7z::buffer_t& bu
             bit7z::buffer_t tmpBuffer{};
             arc.extractTo(tmpBuffer, item.index());
             result.emplace_back(DCOMP{
-                                basePath + "->" + item.path(),
-                                mDepth,
-                                item.size(),
-                                tmpBuffer });
+                basePath + "->" + item.path(),
+                mDepth,
+                tmpBuffer });
         }
     }
     else
     {
-        bit7z::BitArchiveReader arc{ *mLib, buffer, bit7z::BitFormat::Auto };
-        auto arc_items = arc.items();
-        for (const auto& item : arc_items) {
-            if (ValidateFile(item))
-            {
+        try
+        {
+            bit7z::BitArchiveReader arc{ *mLib, buffer, bit7z::BitFormat::Auto };
+            auto arc_items = arc.items();
+            for (const auto& item : arc_items) {
                 bit7z::buffer_t tmpBuffer{};
                 arc.extractTo(tmpBuffer, item.index());
-                auto output = ArcSnp::ResolveBuffer(basePath + "->" + item.path(), tmpBuffer);
-                result.insert(result.end(), output.begin(), output.end());
+                if (CheckBuffer(item))
+                {
+                    auto output = ArcSnp::ResolveBuffer(basePath + "->" + item.path(), tmpBuffer);
+                    result.insert(result.end(), output.begin(), output.end());
+                }
+                else
+                {
+                    result.emplace_back(DCOMP{
+                        basePath + "->" + item.path(),
+                        mDepth,
+                        tmpBuffer });
+                }
             }
+        }
+        catch (const bit7z::BitException&)
+        {
+            result.emplace_back(DCOMP{
+                basePath,
+                mDepth - 1,
+                buffer
+                });
         }
     }
     mDepth--;
     return result;
 }
 
-bool ArcSnp::ValidateFile(const bit7z::BitArchiveItemInfo& itemInfo)
+bool ArcSnp::CheckBuffer(const bit7z::BitArchiveItemInfo& itemInfo)
 {
-    bool state = itemInfo.size() / 1024 < kMaxFileBufferSize;
-    return state;
+    return itemInfo.size() / 1024 <= kMaxFileBufferSize;
+
 }
--- ArchiveSniper/ArchiveSniper.h
+++ ArchiveSniper/ArchiveSniper.h
@@ -2,30 +2,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 #include <bit7z/bitarchivereader.hpp>
 #include <bit7z/bitarchiveeditor.hpp>
-
 
 struct DCOMP
 {
 	std::string msBasePath{};
 	DWORD msDepth{};
-	uint64_t msSize{};
 	bit7z::buffer_t msBuffer{};
 };
+
 using content_t = std::vector<DCOMP>;
 
 struct META
 {
-	uint32_t itemsCount{};
-	uint32_t foldersCount{};
-	uint32_t filesCount{};
-	uint64_t size{};
-	uint64_t packSize{};
-	bit7z::byte_t format{};
+	uint32_t msItemsCount{};
+	uint32_t msFoldersCount{};
+	uint32_t msFilesCount{};
+	uint64_t msSize{};
+	uint64_t msPackSize{};
+	bit7z::byte_t msformat{};
+	std::error_code msErrCode{};
+	std::string msErrMsg{};
 };
-
 
 class ArcSnp
 {
@@ -56,19 +57,56 @@ public:
 	explicit ArcSnp(const std::string& path, bool allowRecursive, DWORD depthLimit = 2);
 	~ArcSnp();
 
-	META GetMetadata(const std::string& filePath, const std::string& logFilePath);
+	META GetMetadata(const std::string& filePath);
+
+	/**********************************************************************
+	* @brief Recursively retrieves the content from the specified path.
+	*
+	* @param path The path to the content.
+	* @return The content extracted from the specified path as a vector of DCOMP (content_t).
+	*			for more info look at DCOMP structure definition
+	*
+	* This method retrieves the buffer of every insider file of one archive from the specified file path,
+	*			supporting recursive extraction for supported compressed formats.
+	**********************************************************************/
 	content_t GetContent(std::string& path);
+
+	/*TODO
 	//std::vector<std::string> GetList(std::string& path);
 	//DWORD RemoveContent(bit7z::buffer_t archBuffer);
-
+	*/
 private:
+
+	/**********************************************************************
+	* @brief Resolves the buffer content and store it into a vector of DCOMP structures (content_t).
+	*
+	* @param basePath : The base path where the buffer is located.
+	*			'->' notation means the buffer/file before it, got opened and anything after '->' is inside that
+	* @param buffer : The buffer to be resolved.
+	* @return The content extracted from the buffer as a content_t.
+	*
+	* This method takes a buffer and its base path, and recursively extracts its content into a content_t.
+	* Each DCOMP structure contains the base path, the depth to show recursion level, and the corresponding buffer for that file.
+	**********************************************************************/
 	content_t ResolveBuffer(const std::string& basePath, bit7z::buffer_t& buffer);
-	bool ValidateFile(const bit7z::BitArchiveItemInfo& itemInfo);
+
+	/**********************************************************************
+	* @brief Checks if the file format of the given item is supported and if its size is under 10 MB.
+	*
+	* This method takes an item (referring to a file) and checks if its format is supported. It then
+	* verifies if the file size is under 10 MB.
+	*
+	* @param itemInfo The information of the file to be checked, including its format and size.
+	* @return DWORD Returns 0 if the file format is supported and its size is under 10 MB.
+	* Returns 1 if the file format is unsupported. Returns 2 if the file size is greater than or equal to 10 MB.
+	**********************************************************************/
+	bool CheckBuffer(const bit7z::BitArchiveItemInfo& itemInfo);
+
 	std::shared_ptr<bit7z::Bit7zLibrary> mLib;
 	bool mRecursion = false;
 	DWORD mDepthLimit;
-	DWORD mDepth{1};
+	DWORD mDepth{0};
 	std::string mPath;
-	const DWORD kBaseArchiveLevel{ 1 };
+	const DWORD kBaseArchiveLevel{ 0 };
 	const DWORD kMaxFileBufferSize{ 10000 };		//10000 KB ~ 10MB
 };
